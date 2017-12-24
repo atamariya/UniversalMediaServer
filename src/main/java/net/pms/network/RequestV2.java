f82af75823e23240cc0f8582db054bf3ccee3b9b
Anand Tamariya <atamariya@gmail.com>
2017-12-24 08:34:22 +0530

    SUBSCRIBE should return HTTP 200

--- src/main/java/net/pms/network/RequestV2.java
+++ src/main/java/net/pms/network/RequestV2.java
@@ -867,31 +867,31 @@ public class RequestV2 extends HTTPResource {
 
 				try {
 					URL soapActionUrl = new URL(cb);
-					String addr = soapActionUrl.getHost();
-					int port = soapActionUrl.getPort();
-					Socket sock = new Socket(addr, port);
-					try (OutputStream out = sock.getOutputStream()) {
-						out.write(("NOTIFY /" + argument + " HTTP/1.1").getBytes());
-						out.write(CRLF.getBytes());
-						out.write(("SID: " + PMS.get().usn()).getBytes());
-						out.write(CRLF.getBytes());
-						out.write(("SEQ: " + 0).getBytes());
-						out.write(CRLF.getBytes());
-						out.write(("NT: upnp:event").getBytes());
-						out.write(CRLF.getBytes());
-						out.write(("NTS: upnp:propchange").getBytes());
-						out.write(CRLF.getBytes());
-						out.write(("HOST: " + addr + ":" + port).getBytes());
-						out.write(CRLF.getBytes());
-						out.flush();
-						sock.close();
-					}
+//					String addr = soapActionUrl.getHost();
+//					int port = soapActionUrl.getPort();
+//					Socket sock = new Socket(addr, port);
+//					try (OutputStream out = sock.getOutputStream()) {
+//						out.write(("NOTIFY /" + argument + " HTTP/1.1").getBytes());
+//						out.write(CRLF.getBytes());
+//						out.write(("SID: " + PMS.get().usn()).getBytes());
+//						out.write(CRLF.getBytes());
+//						out.write(("SEQ: " + 0).getBytes());
+//						out.write(CRLF.getBytes());
+//						out.write(("NT: upnp:event").getBytes());
+//						out.write(CRLF.getBytes());
+//						out.write(("NTS: upnp:propchange").getBytes());
+//						out.write(CRLF.getBytes());
+//						out.write(("HOST: " + addr + ":" + port).getBytes());
+//						out.write(CRLF.getBytes());
+//						out.flush();
+//						sock.close();
+//					}
 				} catch (MalformedURLException ex) {
 					LOGGER.debug("Cannot parse address and port from soap action \"" + soapaction + "\"", ex);
 				}
-			} else {
-				LOGGER.debug("Expected soap action in request");
-			}
+//			} else {
+//				LOGGER.debug("Expected soap action in request");
+			} else
 
 			if (argument.contains("connection_manager")) {
 				response.append(HTTPXMLHelper.eventHeader("urn:schemas-upnp-org:service:ConnectionManager:1"));
@@ -900,11 +900,11 @@ public class RequestV2 extends HTTPResource {
 				response.append(HTTPXMLHelper.eventProp("CurrentConnectionIDs"));
 				response.append(HTTPXMLHelper.EVENT_FOOTER);
 			} else if (argument.contains("content_directory")) {
-				response.append(HTTPXMLHelper.eventHeader("urn:schemas-upnp-org:service:ContentDirectory:1"));
-				response.append(HTTPXMLHelper.eventProp("TransferIDs"));
-				response.append(HTTPXMLHelper.eventProp("ContainerUpdateIDs"));
-				response.append(HTTPXMLHelper.eventProp("SystemUpdateID", "" + DLNAResource.getSystemUpdateId()));
-				response.append(HTTPXMLHelper.EVENT_FOOTER);
+//				response.append(HTTPXMLHelper.eventHeader("urn:schemas-upnp-org:service:ContentDirectory:1"));
+//				response.append(HTTPXMLHelper.eventProp("TransferIDs"));
+//				response.append(HTTPXMLHelper.eventProp("ContainerUpdateIDs"));
+//				response.append(HTTPXMLHelper.eventProp("SystemUpdateID", "" + DLNAResource.getSystemUpdateId()));
+//				response.append(HTTPXMLHelper.EVENT_FOOTER);
 			}
 		} else if (method.equals("NOTIFY")) {
 			output.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/xml");

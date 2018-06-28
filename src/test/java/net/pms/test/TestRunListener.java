6d1dcefd9eaecaa6612fab5d181def36d7ce7a64
Anand Tamariya <atamariya@gmail.com>
2018-06-28 16:13:53 +0530

    Start PMS before test

--- src/test/java/net/pms/test/TestRunListener.java
+++ src/test/java/net/pms/test/TestRunListener.java
@@ -9,6 +9,19 @@ import net.pms.PMS;
 public class TestRunListener extends RunListener {
     @Override
     public void testRunStarted(Description description) throws Exception {
+        System.setProperty("UMS_PROFILE", "target/test-classes/resources");
+        
+//      PmsConfiguration conf = new PmsConfiguration();
+//      RendererConfiguration.loadRendererConfigurations(conf);
+//      PMS.get().setConfiguration(conf);
+//      PMS.get().setRegistry(PMS.createSystemUtils());
+//      PMS.get().setGlobalRepo(new GlobalIdRepo());
+//      PMS.get().refreshLibrary(false);
+//      Tables.checkTables();
+      
+        String[] args = new String[] { "headless" };
+        PMS.main(args);
+
         super.testRunStarted(description);
     }
     
